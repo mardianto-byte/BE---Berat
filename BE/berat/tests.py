8da67d64f2b9f8ee454cd6bf30e45c5dbd274c7e
Mardianto <mardianto@ui.ac.id>
2022-08-13 22:18:56 +0700

    [RED] Implement berat update test

--- BE/berat/tests.py
+++ BE/berat/tests.py
@@ -25,3 +25,21 @@ class TestURL(TestCase):
     def test_berat_list_use_template(self):
         response = Client().get('/berat/list')
         self.assertTemplateUsed(response,'berat_register/berat_list.html')
+    
+    def test_update_berat(self):
+        data = {
+            'tanggal':"2022-08-15",
+            "berat_max": 50,
+            "berat_min": 50,
+        }
+        response = self.client.post('/berat/1/', data)
+        self.assertEqual(response.status_code,302)
+        
+    def test_post_berat(self):
+        data = {
+            'tanggal':"2022-08-15",
+            "berat_max": 50,
+            "berat_min": 50,
+        }
+        response = self.client.post('/berat/0/', data)
+        self.assertEqual(response.status_code,302)
