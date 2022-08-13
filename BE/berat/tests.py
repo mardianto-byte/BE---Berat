83a4923c63455fd6f4094f2c069313af1f13afc4
Mardianto <mardianto@ui.ac.id>
2022-08-13 21:49:46 +0700

    [RED] Add berat form test

--- BE/berat/tests.py
+++ BE/berat/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Berat
 
 # Create your tests here.
 class TestURL(TestCase):
@@ -8,3 +9,13 @@ class TestURL(TestCase):
             berat_max= 50,
             berat_min= 48,
         )
+
+    def test_berat_url_is_exist(self):
+        response = Client().get('/berat/')
+        self.assertEqual(response.status_code,200)
+
+    def test_berat_use_template(self):
+        response = Client().get('/berat/')
+        self.assertTemplateUsed(response,'berat_register/berat_form.html')
+    
+    
