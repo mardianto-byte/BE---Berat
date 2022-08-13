cf586ebbe21409d62004f2dd31a74e7c25d9ef00
Mardianto <mardianto@ui.ac.id>
2022-08-13 21:32:33 +0700

    Create Berat Model

--- BE/berat/models.py
+++ BE/berat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+ 
+class Berat(models.Model):
+    tanggal = models.DateField()
+    berat_max = models.PositiveIntegerField()
+    berat_min = models.PositiveIntegerField()
--- BE/berat/tests.py
+++ BE/berat/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestURL(TestCase):
+    def setUp(self):
+        self.berat = Berat.objects.create(
+            tanggal="2022-08-15",
+            berat_max= 50,
+            berat_min= 48,
+        )
